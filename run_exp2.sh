bcc3d27059630b691d58dc7e68920250892489d0
tiago.blima <tiago.blima02@gmail.com>
2021-02-05 09:53:33 -0300

    more experiments

--- run_exp2.sh
+++ run_exp2.sh
@@ -9,7 +9,7 @@
 ! python preprocess.py -lang_src pt -lang_trg spt -share_vocab -save_data m30k_deen_shr.pkl
 
 ! python train.py -data_pkl m30k_deen_shr.pkl -log m30k_deen_shr -embs_share_weight -proj_share_weight -label_smoothing \
-         -save_model trained -b 128 -warmup 12800 -n_layers 10 -n_head 12 -epoch 30 
+         -save_model trained -b 64 -warmup 12800 -n_layers 8 -n_head 10 -epoch 30
 
 
 ! python translate.py -data_pkl m30k_deen_shr.pkl -model trained.chkpt -output prediction.txt
