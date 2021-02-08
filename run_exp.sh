10e091188342be9e731ec31f36cc66da3422eb30
tiago.blima <tiago.blima02@gmail.com>
2021-02-08 09:34:01 -0300

    running experiments

--- run_exp.sh
+++ run_exp.sh
@@ -5,7 +5,7 @@
 ! python preprocess.py -lang_src pt -lang_trg spt -share_vocab -save_data m30k_deen_shr.pkl
 
 ! python train.py -data_pkl m30k_deen_shr.pkl -log m30k_deen_shr -embs_share_weight -proj_share_weight -label_smoothing \
-         -save_model trained -b 128 -warmup 12800 -epoch 30 -no_cuda
+         -save_model trained -b 128 -warmup 12800 -epoch 30
 
 
 ! python translate.py -data_pkl m30k_deen_shr.pkl -model trained.chkpt -output prediction.txt
