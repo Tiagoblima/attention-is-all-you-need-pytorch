857c55c820299f1621b653fdb0f4e2301985fc78
tiago.blima <tiago.blima02@gmail.com>
2021-02-09 09:09:50 -0300

    update scripts

--- run_exp.sh
+++ run_exp.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-! ssh -t u60699 'cd attention-is-all-you-need-pytorch; exec $SHELL'
+#! ssh -t u60699 'cd attention-is-all-you-need-pytorch; exec $SHELL'
 
-! python preprocess.py -lang_src pt -lang_trg spt -share_vocab -save_data m30k_deen_shr.pkl
+ssh u60699@s001-n004
+
+cd  attention-is-all-you-need-pytorch || exit
 
 ! python train.py -data_pkl m30k_deen_shr.pkl -log m30k_deen_shr -embs_share_weight -proj_share_weight -label_smoothing \
-         -save_model trained -b 128 -warmup 12800 -epoch 30
+         -save_model trained -b 128 -warmup 12800 -epoch 20 -no_cuda
 
 
 ! python translate.py -data_pkl m30k_deen_shr.pkl -model trained.chkpt -output prediction.txt
