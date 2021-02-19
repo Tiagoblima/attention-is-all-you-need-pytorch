2a9fa3eee24639e6d27e26c84462b2537b9883c7
tiago.blima <tiago.blima02@gmail.com>
2021-02-19 11:00:43 -0300

    adding files

--- run_exp3.sh
+++ run_exp3.sh
@@ -8,7 +8,7 @@ cd  attention-is-all-you-need-pytorch || exit
 
 
 ! python train.py -data_pkl m30k_deen_shr.pkl -log logs/exp3_hist -embs_share_weight -proj_share_weight -label_smoothing \
-         -save_model checkpoint/exp3 -b 32 -warmup 12800 -n_layers 10 -n_head 12 -epoch 20 -no_cuda
+         -save_model checkpoint/exp3 -b 32 -warmup 12800 -n_layers 10 -n_head 12 -epoch 1000 -no_cuda
 
 
 ! python translate.py -data_pkl m30k_deen_shr.pkl -model exp3.chkpt -no_cuda -output prediction_exp3.txt
