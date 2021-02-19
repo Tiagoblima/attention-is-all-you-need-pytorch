41eb4b2e1dc7fe52fb0b115cbd5daebe1bff0a28
tiago.blima <tiago.blima02@gmail.com>
2021-02-19 11:02:06 -0300

    adding files

--- run_exp2.sh
+++ run_exp2.sh
@@ -4,7 +4,7 @@
 
 #ssh u60699@s001-n004
 
-cd  attention-is-all-you-need-pytorch || exit
+#cd  attention-is-all-you-need-pytorch || exit
 
 
 ! python train.py -data_pkl m30k_deen_shr.pkl -log logs/exp2_hist -embs_share_weight -proj_share_weight -label_smoothing \
