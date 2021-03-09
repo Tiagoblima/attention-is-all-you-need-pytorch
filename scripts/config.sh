545352ac5bdcadca94e8d480d745df2df3b6e86d
tiago.blima <tiago.blima02@gmail.com>
2021-03-09 09:08:20 -0300

    update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,15 +4,16 @@
 
 #! ssh -t u60699 'cd attention-is-all-you-need-pytorch; exec $SHELL'
 
-ssh u63074@login-2
+#ssh u63074@login-2
 
-cd  attention-is-all-you-need-pytorch || exit
+#cd  attention-is-all-you-need-pytorch || exit
 
 ! pwd
 
-! pip install spacy==2.2.2
+#! pip install spacy==2.2.2
+! pip install -r requirements.txt
 ! python -m spacy download pt
-! pip install torch===1.3.1 torchvision===0.4.2 -f https://download.pytorch.org/whl/torch_stable.html
+#! pip install torch===1.3.1 torchvision===0.4.2 -f https://download.pytorch.org/whl/torch_stable.html
 
 ! wget https://github.com/Tiagoblima/ts-corpus-mt/raw/main/simp_mt.zip
 ! unzip simp_mt.zip
