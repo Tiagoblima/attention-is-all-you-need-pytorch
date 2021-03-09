89df5f76bfc080627ffbee1dc80ea78eb0475012
tiago.blima <tiago.blima02@gmail.com>
2021-03-09 09:17:26 -0300

    dynamically checking cuda availability.

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -7,9 +7,9 @@ ssh u63074@login-2
 cd  attention-is-all-you-need-pytorch || exit
 
 ! python train.py -data_pkl m30k_deen_shr.pkl -log logs/exp1_hist -embs_share_weight -proj_share_weight -label_smoothing \
-         -save_model checkpoint/exp1 -b 32 -warmup 12800 -epoch 50 -no_cuda
+         -save_model checkpoint/exp1 -b 32 -warmup 12800 -epoch 50
 
 
-! python translate.py -data_pkl m30k_deen_shr.pkl -model checkpoint/exp1.chkpt -no_cuda -output prediction_exp1.txt
+! python translate.py -data_pkl m30k_deen_shr.pkl -model checkpoint/exp1.chkpt -output prediction_exp1.txt
 
 exit 0
