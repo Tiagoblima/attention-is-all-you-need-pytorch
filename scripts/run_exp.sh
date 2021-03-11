8f1f88254b918ad8b63abdf280c473abf28280c4
tiago.blima <tiago.blima02@gmail.com>
2021-03-11 13:50:42 -0300

    update run_exp.sh

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -6,10 +6,10 @@ ssh u63074@login-2
 
 cd  attention-is-all-you-need-pytorch || exit
 
-! python train.py -data_pkl m30k_deen_shr.pkl -log logs/exp1_hist -embs_share_weight -proj_share_weight -label_smoothing \
-         -save_model checkpoint/exp1 -b 32 -warmup 12800 -epoch 10
+! python train.py -data_pkl m30k_deen_shr.pkl -log logs/A -embs_share_weight -proj_share_weight -label_smoothing \
+         -save_model checkpoint/A_model -b 32 -warmup 12800 -epoch 10
 
 
-! python translate.py -data_pkl m30k_deen_shr.pkl -model checkpoint/exp1.chkpt -output prediction_exp1.txt
+! python translate.py -data_pkl m30k_deen_shr.pkl -model checkpoint/A_model.chkpt -output A_prediction.txt
 
 exit 0
